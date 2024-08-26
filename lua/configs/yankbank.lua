49fca8439503c00859cd8713d0839e1cec20bac6
caleb yates <caldog20@gmail.com>
2024-08-26 00:57:24 -0500

    Yankbank jump behavior

--- lua/configs/yankbank.lua
+++ lua/configs/yankbank.lua
@@ -1,6 +1,7 @@
 local opts = {
-    persist_type = "memory",
-    sep = "---------------------------------"
+  persist_type = "memory",
+  sep = "---------------------------------",
+  num_behavior = "jump",
 }
 
 return opts
