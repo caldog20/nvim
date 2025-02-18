ceaabac0cdaa473ca7431c786339bb5f91c0f369
caleb yates <caldog20@gmail.com>
2025-02-18 16:41:55 -0600

    New ASCII and some plugin changes

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -33,22 +33,36 @@ M.ui = {
 M.nvdash = {
   load_on_startup = true,
 
+  -- header = {
+  --   "███╗░░██╗██╗░░░██╗██╗███╗░░░███╗",
+  --   "████╗░██║██║░░░██║██║████╗░████║",
+  --   "██╔██╗██║╚██╗░██╔╝██║██╔████╔██║",
+  --   "██║╚████║░╚████╔╝░██║██║╚██╔╝██║",
+  --   "██║░╚███║░░╚██╔╝░░██║██║░╚═╝░██║",
+  --   "╚═╝░░╚══╝░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝",
+  --   "                                ",
+  -- },
+
   header = {
-    "███╗░░██╗██╗░░░██╗██╗███╗░░░███╗",
-    "████╗░██║██║░░░██║██║████╗░████║",
-    "██╔██╗██║╚██╗░██╔╝██║██╔████╔██║",
-    "██║╚████║░╚████╔╝░██║██║╚██╔╝██║",
-    "██║░╚███║░░╚██╔╝░░██║██║░╚═╝░██║",
-    "╚═╝░░╚══╝░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝",
-    "                                ",
+    "                                                                     ",
+    " ██████   █████                   █████   █████  ███                 ",
+    "░░██████ ░░███                   ░░███   ░░███  ░░░                  ",
+    " ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████  ",
+    " ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███ ",
+    " ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███ ",
+    " ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███ ",
+    " █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████",
+    "░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░ ",
+    "                                                                     ",
   },
+
   buttons = {
-    { txt="  Find File", keys="Spc f f", cmd="Telescope find_files" },
-    { txt="󰈚  Recent Files", keys="Spc f o", cmd="Telescope oldfiles" },
-    { txt="󰈭  Live Grep", keys="Spc f w", cmd="Telescope live_grep" },
-    { txt="  Bookmarks", keys="Spc m a", cmd="Telescope marks" },
-    { txt="  Themes", keys="Spc t h", cmd="Telescope themes" },
-    { txt="  Mappings", keys="Spc c h", cmd="NvCheatsheet" },
+    { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+    { txt = "󰈚  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Live Grep", keys = "Spc f w", cmd = "Telescope live_grep" },
+    { txt = "  Bookmarks", keys = "Spc m a", cmd = "Telescope marks" },
+    { txt = "  Themes", keys = "Spc t h", cmd = "Telescope themes" },
+    { txt = "  Mappings", keys = "Spc c h", cmd = "NvCheatsheet" },
   },
 }
 
