9fd86fe9cbef288eeba28320ef20224c3319b43e
caleb yates <caldog20@gmail.com>
2026-05-23 23:10:38 -0500

    add substitute plugin and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,13 @@ vim.keymap.set("n", "Q", "<nop>", { remap = true })
 vim.keymap.set("n", "q", "<NOP>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>d", [["_dd]])
 vim.keymap.set("v", "<leader>d", [["_d]])
+
+-- substitute
+vim.keymap.set("n", "s", require("substitute").operator, { noremap = true })
+vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
+vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
+vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
+vim.keymap.set("n", "sx", require("substitute.exchange").operator, { noremap = true })
+vim.keymap.set("n", "sxx", require("substitute.exchange").line, { noremap = true })
+vim.keymap.set("x", "X", require("substitute.exchange").visual, { noremap = true })
+vim.keymap.set("n", "sxc", require("substitute.exchange").cancel, { noremap = true })
