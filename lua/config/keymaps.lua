c3c5c174ae6256899ef5c418cd27c89eccae01f1
caleb yates <caldog20@gmail.com>
2026-06-10 13:02:35 -0500

    add proper description fields to keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,18 +9,23 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u", "<C-u>zz")
 vim.keymap.set("n", "Q", "<nop>", { remap = true })
 vim.keymap.set("n", "q", "<NOP>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>d", [["_dd]])
-vim.keymap.set("v", "<leader>d", [["_d]])
+vim.keymap.set("n", "<leader>dd", [["_dd]], { desc = "Delete noreg" })
+vim.keymap.set("v", "<leader>dd", [["_d]], { desc = "Delete visual noreg" })
 
 -- substitute
-vim.keymap.set("n", "s", require("substitute").operator, { noremap = true })
-vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
-vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
-vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
-vim.keymap.set("n", "sx", require("substitute.exchange").operator, { noremap = true })
-vim.keymap.set("n", "sxx", require("substitute.exchange").line, { noremap = true })
-vim.keymap.set("x", "X", require("substitute.exchange").visual, { noremap = true })
-vim.keymap.set("n", "sxc", require("substitute.exchange").cancel, { noremap = true })
+vim.keymap.set("n", "s", require("substitute").operator, { desc = "substitute", noremap = true })
+vim.keymap.set("n", "ss", require("substitute").line, { desc = "substitute line", noremap = true })
+vim.keymap.set("n", "S", require("substitute").eol, { desc = "substitute eol", noremap = true })
+vim.keymap.set("x", "s", require("substitute").visual, { desc = "substitute visual", noremap = true })
+vim.keymap.set("n", "sx", require("substitute.exchange").operator, { desc = "substitute exchange", noremap = true })
+vim.keymap.set("n", "sxx", require("substitute.exchange").line, { desc = "substitute exchange line", noremap = true })
+vim.keymap.set("x", "X", require("substitute.exchange").visual, { desc = "substitute exchange visual", noremap = true })
+vim.keymap.set(
+  "n",
+  "sxc",
+  require("substitute.exchange").cancel,
+  { desc = "substitute exchange cancel", noremap = true }
+)
 
 -- Runs 'go mod tidy' and shows live progress via Snacks notifier
 vim.keymap.set("n", "<leader>ct", function()
