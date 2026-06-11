b88541c5b2db75b2b8ce6717720d2eb91cc23a97
caleb yates <caldog20@gmail.com>
2026-06-11 16:57:25 -0500

    add LspInfo command shortcut and set line wrapping

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,13 @@ vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 vim.opt.relativenumber = false
 vim.g.omni_sql_no_default_maps = 1
+
+vim.opt.wrap = true
+vim.opt.linebreak = true -- break at word boundaries, not mid-token
+vim.opt.breakindent = true -- wrapped lines maintain indentation
+vim.opt.showbreak = "↪ " -- visual indicator for wrapped lines
+vim.opt.colorcolumn = "120"
+-- LspInfo -> checkhealth vim.lsp
+vim.api.nvim_create_user_command("LspInfo", function()
+  vim.cmd("silent checkhealth vim.lsp")
+end, {})
